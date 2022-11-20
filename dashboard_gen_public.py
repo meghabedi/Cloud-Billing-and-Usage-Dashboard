9c530f45de775f63c0d65d2751c9a1562ea2da54
meghabedi <meghabedi990@gmail.com>
2022-11-20 19:47:04 +0530

    Added Public Dataset

--- dashboard_gen_public.py
+++ dashboard_gen_public.py
@@ -163,11 +163,6 @@ def create_billboard_view(args, isStandard):
                   " so skipping billboard detailed view creation")
         return
 
-    '''sql = """
-    CREATE VIEW if not exists `{}`
-    AS select *, COALESCE((SELECT SUM(x.amount) FROM UNNEST(s.credits) x),0) AS credits_sum_amount, COALESCE((SELECT SUM(x.amount) FROM UNNEST(s.credits) x),0) + cost as net_cost, EXTRACT(DATE FROM _PARTITIONTIME) AS date from `{}` s WHERE _PARTITIONTIME >'2020-08-01'
-    """.format(view_id, source_id) . '''
-
     sql = """CREATE VIEW if not exists `{}`
 AS
 WITH
@@ -198,7 +193,7 @@ WITH
     invoice,
     cost_type
   FROM
-    `{}`
+    `data-analytics-pocs.public.billing_dashboard_export`
   WHERE
     usage_start_time >= '2020-12-01'
     AND usage_end_time <= CURRENT_TIMESTAMP()),
@@ -234,7 +229,342 @@ UNION ALL
 SELECT
   credits.*
 FROM
-  credits""".format(view_id, source_id)
+  credits""".format(view_id)
+
+
+    # Not sure why this need project_id
+    bq_view_client = bigquery.Client(project=args.PROJECT_ID)
+
+    job = bq_view_client.query(sql)  # API request.
+    job.result()  # Waits for the query to finish.
+
+    if isStandard is True:
+        output_url = report_base_url + standard_view_url.format(
+            args.PROJECT_ID, args.BILLBOARD_DATASET_NAME_TO_BE_CREATED,
+            args.bb_standard)
+    else:
+        output_url = output_url + detailed_view_url.format(
+            args.PROJECT_ID, detailedBBDataset, args.bb_detailed)
+
+    print('Created view {}{}.{}.{}'.format(Back.GREEN, job.destination.project,
+                                           job.destination.dataset_id,
+                                           job.destination.table_id))
+    print(Style.RESET_ALL)
+
+
+def generate_datastudio_url(args):
+    print(
+        "To view dataset, please click " + Back.GREEN +
+        "https://console.cloud.google.com/bigquery", "\n")
+
+    print(Style.RESET_ALL)
+
+    print("To launch datastudio report, please click " + Back.GREEN +
+          output_url + "\n")
+    print(Style.RESET_ALL)
+
+
+def remove_billboard_dataset(args):
+    standard_view_id = "{}.{}.{}".format(
+        args.PROJECT_ID, args.BILLBOARD_DATASET_NAME_TO_BE_CREATED,
+        args.bb_standard)
+    bq_client.delete_table(standard_view_id, not_found_ok=True)
+    print("Billboard view {} deleted.".format(standard_view_id))
+    detailed_view_id = "{}.{}.{}".format(
+        args.PROJECT_ID, args.BILLBOARD_DATASET_NAME_TO_BE_CREATED,
+        args.bb_detailed)
+    bq_client.delete_table(detailed_view_id, not_found_ok=True)
+    print("Billboard view {} deleted.".format(detailed_view_id))
+    return True
+
+
+def main(argv):
+
+    global detailedBBDataset
+    parser = argparse.ArgumentParser(
+        description='Billing Export information, Version=' + app_version)
+    parser.add_argument('-v',
+                        action='version',
+                        version='Version of %(prog)s ' + app_version)
+
+    parser.add_argument('-pr',
+                        dest='PROJECT_ID',
+                        type=str,
+                        help='Project Id',
+                        required=True)
+    parser.add_argument('-se',
+                        dest='STANDARD_BILLING_EXPORT_DATASET_NAME',
+                        type=str,
+                        required=True)
+    parser.add_argument('-de',
+                        dest='DETAILED_BILLING_EXPORT_DATASET_NAME',
+                        type=str,
+                        required=True)
+
+    parser.add_argument('-bb',
+                        dest='BILLBOARD_DATASET_NAME_TO_BE_CREATED',
+                        type=str,
+                        required=True)
+
+    parser.add_argument('-clean',
+                        dest='clean',
+                        type=str,
+                        help='Only when you need cleanup, provide "yes"')
+
+    args = parser.parse_args()
+    print('Version of billboard.py  ' + app_version + "\n")
+
+    if args.DETAILED_BILLING_EXPORT_DATASET_NAME is None:
+        print("Detailed export not provided so setting default to Standard.")
+        args.DETAILED_BILLING_EXPORT_DATASET_NAME = args.STANDARD_BILLING_EXPORT_DATASET_NAME
+
+    Welcome to Cloud Shell! Type "help" to get started.
+Your Cloud Platform project in this session is set to positive-water-366914.
+Use “gcloud config set project [PROJECT_ID]” to change to a different project.
+bedii@cloudshell:~ (positive-water-366914)$ ls
+bill-env  Cloud-Billing-and-Usage-Dashboard  cloudshell_open  cloud_sql_proxy  README-cloudshell.txt
+bedii@cloudshell:~ (positive-water-366914)$ cd Cloud-Billing-and-Usage-Dashboard
+bedii@cloudshell:~/Cloud-Billing-and-Usage-Dashboard (positive-water-366914)$ ls
+bill-env  dashboard_gen.py  README.md  requirements.txt
+bedii@cloudshell:~/Cloud-Billing-and-Usage-Dashboard (positive-water-366914)$ cat dashboard_gen.py
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from google.cloud import bigquery
+from google.cloud import billing
+from google.api_core.exceptions import PermissionDenied
+from google.cloud.exceptions import NotFound
+import argparse
+import sys
+from colorama import Back
+from colorama import Style
+
+bq_client = bigquery.Client()
+#https://datastudio.google.com/c/u/0/reporting/7780ebbd-b1af-402c-92f2-d9a6ffcc22fb/page/vSVyB?c.reportId=2e2ea000-8f68-40e2-8847-b80f05069b6e&r.reportName=MyBillboard&ds.ds39.connector=bigQuery&ds.ds39.projectId=positive-water-366914&ds.ds39.type=TABLE&ds.ds39.datasetId=Billing_Dashboard&ds.ds39.tableId=billboard&ds.ds93.connector=bigQuery&ds.ds93.projectId=positive-water-366914&ds.ds93.type=TABLE&ds.ds93.datasetId=Billing_Dashboard&ds.ds93.tableId=billboard_detail
+base_url = "https://datastudio.google.com/reporting/create?"
+report_part_url = base_url + "c.reportId=afc93bd5-2fd9-4682-a083-0494071cc858"
+report_base_url = report_part_url + "&r.reportName=BillingUsage"
+
+std_proj_url = "&ds.ds0.connector=bigQuery&ds.ds0.projectId={}"
+std_table_url = "&ds.ds0.type=TABLE&ds.ds0.datasetId={}&ds.ds0.tableId={}"
+standard_view_url = std_proj_url + std_table_url
+
+dtl_proj_url = "&ds.ds2.connector=bigQuery&ds.ds2.projectId={}"
+dtl_table_url = "&ds.ds2.type=TABLE&ds.ds2.datasetId={}&ds.ds2.tableId={}"
+detailed_view_url = dtl_proj_url + dtl_table_url
+
+output_url = ""
+isDetailedExportDifferentLocation = False
+detailedBBDataset = ""
+
+app_version = "2.0"
+
+
+# This function checks if billboard dataset already exists or not
+# so that we are not recreating it
+def check_billboard_dataset_exists(dataset_id):
+    try:
+        bq_client.get_dataset(dataset_id)  # Make an API request.
+        print("Dataset {} already exists.".format(dataset_id))
+        return True
+    except NotFound:
+        print("Dataset {} is not found.".format(dataset_id))
+        return False
+
+
+# Creates billboard dataset.
+# Location is taken from the billing export table provided by the user.
+def create_dataset(args):
+
+    global detailedBBDataset
+    standard_source_id = "{}.{}.{}".format(
+        args.PROJECT_ID, args.STANDARD_BILLING_EXPORT_DATASET_NAME,
+        args.standard_table)
+
+    detailed_source_id = "{}.{}.{}".format(
+        args.PROJECT_ID, args.DETAILED_BILLING_EXPORT_DATASET_NAME,
+        args.detailed_table)
+
+    standard_table_info = None
+    detailed_table_info = None
+
+    print("Creating standard Dataset.")
+    try:
+        standard_table_info = bq_client.get_table(standard_source_id)
+        print("Exported {} in GEO Location={}".format(
+            standard_source_id, standard_table_info.location))
+        # Create dataset for BB for standard export.
+        dataset_id = "{}.{}".format(args.PROJECT_ID,
+                                    args.BILLBOARD_DATASET_NAME_TO_BE_CREATED)
+        create_dataset_by_location(dataset_id, standard_table_info.location)
+    except NotFound:
+        print("Table {} is not found check the export and proceed.".format(
+            standard_source_id))
+        # Standard is mandatory so program will fail if doesnot exists.
+        sys.exit()
+
+    if args.DETAILED_BILLING_EXPORT_DATASET_NAME is None:
+        return True
+
+    print("Creating detailed Dataset.")
+
+    try:
+        detailed_table_info = bq_client.get_table(detailed_source_id)
+        print("Exported {} in GEO Location={}".format(
+            detailed_source_id, detailed_table_info.location))
+        # Check if detailed export is in different location.
+        if standard_table_info.location != detailed_table_info.location:
+            detailedBBDataset = '{}_detail'.format(
+                args.BILLBOARD_DATASET_NAME_TO_BE_CREATED)
+            dataset_id = "{}.{}".format(args.PROJECT_ID, detailedBBDataset)
+            print("Creating another dataset {} in detailed export loc".format(
+                dataset_id))
+            create_dataset_by_location(dataset_id, detailed_table_info.location)
+
+    except NotFound:
+        print("Table {} is not found check the export.".format(
+            detailed_source_id))
+        # Skip failing if detailed is not there.
+        # sys.exit()
+
+
+# Creates billboard dataset based on billing exported location
+# Location is taken from the billing export table provided by the user.
+def create_dataset_by_location(dataset_id, location):
+    # Check if billboard dataset exists
+    if check_billboard_dataset_exists(dataset_id) is True:
+        return
+    # Since we need to create, construct a full
+    # Dataset object to send to the API.
+    dataset = bigquery.Dataset(dataset_id)
+    dataset.location = location
+    # Send the dataset to the API for creation, with an explicit timeout.
+    # Raises google.api_core.exceptions.Conflict if the Dataset already
+    # exist within the project.
+    # Make an API request.
+    dataset = bq_client.create_dataset(dataset, timeout=30)
+    print("Created dataset {} on location {}".format(dataset_id, location))
+
+
+# Creates the view for the Billboard
+def create_billboard_view(args, isStandard):
+
+    global output_url
+    global detailedBBDataset
+
+    if isStandard is True:
+        source_id = "{}.{}.{}".format(args.PROJECT_ID,
+                                      args.STANDARD_BILLING_EXPORT_DATASET_NAME,
+                                      args.standard_table)
+        view_id = "{}.{}.{}".format(args.PROJECT_ID,
+                                    args.BILLBOARD_DATASET_NAME_TO_BE_CREATED,
+                                    args.bb_standard)
+    else:
+        source_id = "{}.{}.{}".format(args.PROJECT_ID,
+                                      args.DETAILED_BILLING_EXPORT_DATASET_NAME,
+                                      args.detailed_table)
+        view_id = "{}.{}.{}".format(args.PROJECT_ID, detailedBBDataset,
+                                    args.bb_detailed)
+
+    print('source_id={} and view_id={}'.format(source_id, view_id))
+
+    # Standard -Fail view creation & url construct if standard is missing
+    # Detailed -Skip view creation & url construct if detailed is missing.
+    try:
+        bq_client.get_table(source_id)
+    except NotFound:
+        if isStandard is True:
+            print("Standard usage cost export not found=" + source_id +
+                  " so skipping billboard view creation")
+            sys.exit()
+        else:
+            print("Detailed usage cost export not found=" + source_id +
+                  " so skipping billboard detailed view creation")
+        return
+
+    '''sql = """
+    CREATE VIEW if not exists `{}`
+    AS select *, COALESCE((SELECT SUM(x.amount) FROM UNNEST(s.credits) x),0) AS credits_sum_amount, COALESCE((SELECT SUM(x.amount) FROM UNNEST(s.credits) x),0) + cost as net_cost, EXTRACT(DATE FROM _PARTITIONTIME) AS date from `{}` s WHERE _PARTITIONTIME >'2020-08-01'
+    """.format(view_id, source_id) . '''
+
+    sql = """CREATE VIEW if not exists `{}`
+AS
+WITH
+  billing_data AS (
+  SELECT
+    billing_account_id,
+    service,
+    sku,
+    usage_start_time,
+    usage_end_time,
+    STRUCT( coalesce(
+      IF
+        (service.description='Support',
+          'Support',
+          project.id),
+        "Unknown") AS id,
+      project.name,
+      project.ancestry_numbers) AS project,
+    project.labels AS project_labels,
+    labels AS resource_labels,
+    system_labels,
+    location,
+    cost,
+    credits,
+    currency,
+    currency_conversion_rate,
+    usage,
+    invoice,
+    cost_type
+  FROM
+    `data-analytics-pocs.public.billing_dashboard_export`
+  WHERE
+    usage_start_time >= '2021-01-01'
+    AND usage_end_time <= '2022-12-31'),
+  -- Costs data
+  costs AS (
+  SELECT
+    ' Gross' AS cost_metric_type,
+    cost,
+    0 AS credit_amount,
+    billing_data.* EXCEPT(credits, cost)
+  FROM
+    billing_data ),
+  -- Credits data
+  credits AS (
+  SELECT
+    c.name AS cost_metric_type,
+    0 AS cost,
+    c.amount AS credit_amount,
+    billing_data.* EXCEPT(credits,
+      cost)
+  FROM
+    billing_data
+  JOIN
+    UNNEST(credits) c
+  WHERE
+    c.name IS NOT NULL )
+-- Query costs and credits
+SELECT
+  costs.*
+FROM
+  costs
+UNION ALL
+SELECT
+  credits.*
+FROM
+  credits""".format(view_id)
 
 
     # Not sure why this need project_id
@@ -341,10 +671,12 @@ def main(argv):
         "/")[1]
 
     print("Project billing account=" + billing_account_name, "\n")
-    args.standard_table = "gcp_billing_export_v1_" + \
+    '''args.standard_table = "gcp_billing_export_v1_" + \
         billing_account_name.replace('-', '_')
     args.detailed_table = "gcp_billing_export_resource_v1_" + \
-        billing_account_name.replace('-', '_')
+        billing_account_name.replace('-', '_')'''
+    args.standard_table = "billing_dashboard_export"
+    args.detailed_table = "billing_dashboard_export"
     args.bb_standard = "billboard"
     args.bb_detailed = "billboard_detail"
 
